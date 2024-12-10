80107ebcb14f886947d5c484a9c1bd3970dc9a32
thomaskummer1 <twk23@outlook.com>
2024-12-10 02:24:35 -0500

    first commit

---
+++ Users/routes.js
@@ -0,0 +1,62 @@
+import * as dao from './dao.js';
+
+export default function UserRoutes(app) {
+    const createUser = async (req, res) => {
+        const user = await dao.createUser(req.body);
+        res.json(user);
+    };
+    app.post('/api/users', createUser);
+    const deleteUser = async (req, res) => {
+        const status = await dao.deleteUser(req.params.userId);
+        res.json(status);
+    };
+    app.delete('/api/users/:userId', deleteUser);
+    const findUserById = async (req, res) => {
+        const user = await dao.findUserById(req.params.userId);
+        res.json(user);
+    };
+    app.get('/api/users/:userId', findUserById);
+    const updateUser = async (req, res) => {
+        const status = await dao.updateUser(req.params.userId, req.body);
+        currentUser = await dao.findUserById(req.params.userId);
+        res.json(currentUser);
+    };
+    app.put('/api/users/:userId', updateUser);
+    const signup = async (req, res) => {
+        const user = await dao.findUserByUsername(req.body.username);
+        if (user) {
+          res.status(400).json({ message: "Username already taken" });
+          return;
+        }
+        console.log(req.body);
+        const currentUser = await dao.createUser(req.body);
+        req.session["currentUser"] = currentUser;
+        res.json(currentUser);
+    };
+    app.post("/api/users/signup", signup);
+    const signin = async (req, res) => {
+        const { username, password } = req.body;
+        const currentUser = await dao.findUserByCredentials(username, password);
+        if (currentUser) {
+          req.session["currentUser"] = currentUser;
+          res.json(currentUser);
+        } else {
+          res.status(401).json({ message: "Unable to login. Try again later." });
+        }
+    };
+    app.post("/api/users/signin", signin);
+    const signout = (req, res) => {
+        req.session.destroy();
+        res.sendStatus(200);
+    };
+    app.post("/api/users/signout", signout);
+    const profile = (req, res) => {
+        const currentUser = req.session["currentUser"];
+        if (!currentUser) {
+          res.sendStatus(401);
+          return;
+        }
+        res.json(currentUser);
+    };
+    app.post("/api/users/profile", profile);
+}
---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+import session from 'express-session'
+import UserRoutes from './Users/routes.js'
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/innerMusic";
+mongoose.connect(CONNECTION_STRING);
+const app = express()
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    })
+   ); 
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "music",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.NODE_SERVER_DOMAIN,
+    };
+}
+app.use(session(sessionOptions));
+app.use(cors())
+app.use(express.json())
+UserRoutes(app)
+app.listen(process.env.PORT || 4000);
