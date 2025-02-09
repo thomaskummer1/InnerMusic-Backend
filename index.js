87eb689df7d5f38630f3747836f1afc1af8581f5
thomaskummer1 <twk23@outlook.com>
2025-02-08 19:35:21 -0500

    reverted cors changes

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV !== "development") {
     sessionOptions.proxy = true;
     sessionOptions.cookie = {
       sameSite: "none",
-      secure: false,
+      secure: true,
       domain: process.env.NODE_SERVER_DOMAIN,
     };
 }
